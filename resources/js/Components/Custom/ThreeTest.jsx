184dd820a58042e8b0d0b47d801853a4057dd047
Luna Muylkens <93606228+LunashaGit@users.noreply.github.com>
2022-12-29 16:48:22 +0100

    Add Test ThreeJS

--- resources/js/Components/Custom/ThreeTest.jsx
+++ resources/js/Components/Custom/ThreeTest.jsx
@@ -1,39 +1,38 @@
 import * as THREE from "three";
 import React, { useEffect, useRef } from "react";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 export default function ThreeTest() {
-    const mount = useRef(null);
-
+    const containerRef = useRef(null);
     useEffect(() => {
-        let scene = new THREE.Scene();
-        let camera = new THREE.PerspectiveCamera(
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera(
             75,
             window.innerWidth / window.innerHeight,
             0.1,
             1000
         );
-
-        let renderer = new THREE.WebGLRenderer();
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        mount.current.appendChild(renderer.domElement);
-
-        let geometry = new THREE.BoxGeometry();
-        let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-        let cube = new THREE.Mesh(geometry, material);
+        const controls = new OrbitControls(camera, containerRef.current);
+        const renderer = new THREE.WebGLRenderer();
+        const geometry = new THREE.BoxGeometry(10, 1, 10);
+        const geometry2 = new THREE.BoxGeometry(1, 10, 10);
+        const material = new THREE.MeshBasicMaterial({ color: "red" });
+        const cube = new THREE.Mesh(geometry, material);
         scene.add(cube);
-
+        const cube2 = new THREE.Mesh(geometry2, material);
+        scene.add(cube2);
         camera.position.z = 5;
 
-        const animate = () => {
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        containerRef.current.appendChild(renderer.domElement);
+        function animate() {
             requestAnimationFrame(animate);
-
-            cube.rotation.x += 0.01;
-            cube.rotation.y += 0.01;
-
+            controls.update();
             renderer.render(scene, camera);
-        };
+        }
+
         animate();
     }, []);
 
-    return <div ref={mount} />;
+    return <div ref={containerRef} />;
 }
