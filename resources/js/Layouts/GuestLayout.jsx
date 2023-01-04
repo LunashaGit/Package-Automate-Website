396129716d596503b1a2b831d676c4075e34c05c
LunashaGit <lunamuylkens@gmail.com>
2023-01-04 23:34:37 +0100

    Go back Register and Login Page ( Guest Layout ) + Change Favicon

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -5,7 +5,9 @@ export default function Guest({ children }) {
     return (
         <div className="background min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div className="bg-white w-full sm:max-w-md mt-6 px-6 py-4 shadow-md p-4 sm:rounded-lg">
-                <ApplicationLogo className="w-32 h-32 p-4 m-auto fill-current text-gray-500 bg-white rounded-full mt-moins5" />
+                <Link href="/">
+                    <ApplicationLogo className="w-32 h-32 p-4 m-auto fill-current text-gray-500 bg-white rounded-full mt-moins5" />
+                </Link>
                 {children}
             </div>
         </div>
